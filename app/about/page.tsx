9568be81ba19361dfeef972b46615f21c0257888
mbilal2913 <mbilal2913@gmail.com>
2025-10-08 09:08:25 +0000

    fix error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,14 @@
-import Image from "next/Image";
+import Image from "next/image";
 
 export default function AboutPage() {
     return (
         <>
         <div className="flex gap-3">
             <div>
-                <Image src="https://placehold.co/400" />
+                <Image src="https://placehold.co/400" alt="Portrait of Bilal"/>
             </div>
             <div>
-                I'm Bilal, a student and pationate about new techs
+                {`I'm Bilal, a student and pationate about new techs`}
             </div>
         </div>
         </>
